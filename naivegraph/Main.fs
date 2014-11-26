82fe777003c3e6424776639284a902c40302540f
victor-dumitrescu <s1107546@sms.ed.ac.uk>
2014-11-26 20:42:40 +0000

    Generate graphs for all processed scripts

--- naivegraph/Main.fs
+++ naivegraph/Main.fs
@@ -6,12 +6,19 @@ open ToGEXF
 
 open System.IO
 
-[<EntryPoint>]
-let main argv = 
-    
+let absPath = "C:/Users/Victor/GitHub/base/"
+let dirs = absPath
+           |> Directory.GetDirectories 
+           |> Array.filter (fun x -> match Array.tryFind (fun y -> printfn "%A" y
+                                                                   y = x+"/processed\script.xml") (Directory.GetFiles (x+"/processed")) with
+                                     | Some _ -> true
+                                     | None -> false)
+
+let constructGraph (path: string) = 
+
     let graph = new CharGraph()
 
-    let scenes = "C:/Users/Victor/GitHub/base/The Silence of the Lambs (film)/processed/script.xml" 
+    let scenes = path + "/processed/script.xml" 
                  |> File.ReadAllText
                  |> Script.Parse
                  |> (fun x -> x.Scenes)
@@ -21,6 +28,12 @@ let main argv =
     List.map (addChar graph) scenes |> ignore
 
     let output = constructXML graph
-    File.WriteAllLines(sprintf "%s%s" (Directory.GetCurrentDirectory()) "/../../../data/naivegraph.gexf", [|output|])
+    let name = sprintf "/../../../output/%s.gexf" (path.[absPath.Length..].Replace(' ', '_'))
+    File.WriteAllLines(sprintf "%s%s" (Directory.GetCurrentDirectory()) name, [|output|])
 
+
+[<EntryPoint>]
+let main argv = 
+    
+    Array.map constructGraph dirs |> ignore
     0
